dc8fa1d47f377d396270daf354f54f23b4661348
Savchenko Illya <74146705+SavchenkoIllya@users.noreply.github.com>
2023-02-03 10:42:54 +0100

    Update Symbols.ts

--- src/app/login/Symbols.ts
+++ src/app/login/Symbols.ts
@@ -4,9 +4,12 @@ export class Symbols {
 
   static letters = new RegExp(/^[a-zA-Z]+$/);
   static numbers = new RegExp(/^[0-9]+$/);
-  static symbols = new RegExp(/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/);
+  static symbols = new RegExp(/^[!@#$%^&*()_+\-=~`\[\]{};':"\\|,.<>\/?]+$/);
   static lettersNumbers = new RegExp(/^[a-zA-Z0-9]+$/);
-  static lettersSymbols = new RegExp(/^[a-zA-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/);
-  static numbersSymbols = new RegExp(/^[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/);
-  static all = new RegExp(/^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/);
+  static lettersSymbols = new RegExp(/^[a-zA-Z!@#$%^&*()_+\-=~`\[\]{};':"\\|,.<>\/?]+$/);
+  static numbersSymbols = new RegExp(/^[0-9!@#$%^&*()_+\-=~`\[\]{};':"\\|,.<>\/?]+$/);
+  static allAllowed = new RegExp(/^[a-zA-Z0-9!@#$%^&*()_+\-=~`\[\]{};':"\\|,.<>\/?]+$/);
+
+  //all that doesn't included in allAllowed
+  static else = new RegExp(/[^a-zA-Z0-9!@#$%^&*()_+\-=~`[\]{};':"\\|,.<>/?]/g);
 }
